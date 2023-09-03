cf46c2e635aa157d9137ad5f6b2d604b5136936a
Chris <chris@Chriss-MacBook-Air-2.local>
2023-09-03 14:27:38 -0400

    remade paper plots for candidacy exam

--- src/mastrangelo/simulate_helpers.py
+++ src/mastrangelo/simulate_helpers.py
@@ -93,7 +93,7 @@ def compute_prob_vectorized(df, m, b, cutoff, bootstrap): # adapted from Ballard
     - df: same as input, but with new column called prob_intact
 
     """
-
+    
     if bootstrap == False:
         df['prob_intact'] = np.where(
                 ((df['iso_age']*1e9 > 1e8) & (df['iso_age']*1e9 <= cutoff)), b+m*(np.log10(df['iso_age']*1e9)-8), np.where(
